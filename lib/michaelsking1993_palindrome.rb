dff84b9142a4a1d9464a14f2f6b58161b0545679
Michael King <michaelsking1993@gmail.com>
2019-04-08 23:40:13 -0500

    Finish working and refactored palindrome method

--- lib/michaelsking1993_palindrome.rb
+++ lib/michaelsking1993_palindrome.rb
@@ -1,5 +1,21 @@
 require "michaelsking1993_palindrome/version"
 
-module Michaelsking1993Palindrome
-  # Your code goes here...
+class String
+
+# Returns true for a palindrome, false otherwise.
+def palindrome?
+  processed_content == processed_content.reverse
+end
+
+# Returns the letters in the string.
+def letters
+  self.chars.select { |c| c.match(/[a-z]/i) }.join
+end
+
+private
+
+# Returns content for palindrome testing.
+def processed_content
+  self.letters.downcase
+end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "michaelsking1993_palindrome"
 
 require "minitest/autorun"
+
+require "minitest/reporters"
+Minitest::Reporters.use!
